bc5183963ca693edb967d04d1340893094b20231
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-03-25 17:53:55 -0700

    migration error

--- src/app.js
+++ src/app.js
@@ -4,28 +4,24 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const router = require('./router')
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
 app.use(helmet())
+app.use(validateBearerToken)
+
+app.use(router)
 
 app.get('/', (req, res) => {
-  res.send('Hello, boilerplate! test')
-});
-
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error);
-    response = { message: error.message, error}
-  }
+  res.send('Hello, world!')
 })
-module.exports = app;
+
+app.use(errorHandler)
+
+module.exports = app
