b06515e93fd8d32457a898aadb8ce115c3ad0ef2
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-04-03 16:20:17 -0700

    2 out of 9 tests failing, debug

--- test/notes.fixtures.js
+++ test/notes.fixtures.js
@@ -8,21 +8,21 @@ function makeNotesArray() {
       title: "Dogs",
       modified:"2019-01-03T00:00:00.000Z",
       content: "content",
-      folderid: 4
+      folderid: 1
     },
     {
       id: 2,
       title: "Cats",
       modified:"2018-08-15T23:00:00.000Z",
       content: "content",
-      folderid: 5
+      folderid: 2
     },
     {
       id: 3,
       title: "Pigs",
       modified:"2018-03-01T00:00:00.000Z",
       content: "content",
-      folderid: 6
+      folderid: 3
     },
   ];
 }
