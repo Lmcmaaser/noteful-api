829405333455397b0e7bf5be9be958797088bc85
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-04-22 16:48:56 -0700

    production debug

--- src/logger.js
+++ src/logger.js
@@ -3,14 +3,14 @@ const { NODE_ENV } = require('./config')
 
 const logger = winston.createLogger({
   // log everything with a severity of info and greater
-  level: 'info',
+  level: 'debug',
   format: winston.format.json(),
   transports: [
     new winston.transports.File({ filename: 'info.log' })
   ]
 });
 
-if (!['test'].includes(NODE_ENV)) {
+if (!['production', 'test'].includes(NODE_ENV)) {
   logger.add(new winston.transports.Console({
     format: winston.format.simple()
   }))
--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -5,6 +5,7 @@ function validateBearerToken(req, res, next) {
 const authToken = req.get('Authorization')
 
 if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+  logger.error(`Unauthorized request to path: ${req.path}`);
   return res.status(401).json({ error: 'Unauthorized request' })
 }
 
